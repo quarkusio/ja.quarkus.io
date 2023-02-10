023ff5fe49f20a076d9cfb895a1cd74efb9769bf
Yoshikazu Nojima <mail@ynojima.net>
2023-02-10 10:01:15 +0900

    Add message to tmx_manager.py

--- lib/doc_l10n_kit/tmx_manager.py
+++ lib/doc_l10n_kit/tmx_manager.py
@@ -37,6 +37,7 @@ class TmxManager:
         file_paths = [item for item in items if os.path.isdir(item) == False]
         concurrent.futures.ThreadPoolExecutor(thread_name_prefix="worker").map(lambda file_path : self.__apply_po_filter(tempdir, file_path), file_paths)
         subprocess.run("find $TMP/l10n/po -name \"*.po\" | xargs msgcat --force-po --to-code=utf-8 --no-wrap --use-first -o {}/accumulation.po -i {}/accumulation.po".format(tempdir, tempdir), shell=True, check=True)
+        print("Generating {}".format(self.__tmx_file_pat))
         subprocess.run("grep -v \"^#~\" {}/accumulation.po > {}/comment-filtered.po".format(tempdir, tempdir), shell=True, check=True)
         subprocess.run("po2tmx -l {} -i {}/comment-filtered.po -o {}".format(self.__target_lang, tempdir, self.__tmx_file_path), shell=True, check=True)
         self.sort()
@@ -48,27 +49,3 @@ class TmxManager:
         subprocess.run("pofilter --nonotes --nofuzzy --excludefilter=untranslated -i {} -o {}/{}".format(file_path, tempdir, file_path), shell=True, check=True)
         pathlib.Path("{}/{}".format(tempdir, file_path)).touch()
 
-
-
-# export TARGET_LANG=`jq ".targetLang" config/l10n-kit.json -r`
-# export TMX_FILE_PATH=`jq ".tmx.filePath" config/l10n-kit.json -r`
-#
-# mkdir -p l10n/tmx
-# TMP=`mktemp -d`
-# touch $TMP/accumulation.po
-#
-# for ITEM in `\find l10n/po -name "*.po"`; do
-# if [ -f $ITEM ] ; then
-# echo $TMP/$ITEM
-# mkdir -p $(dirname $TMP/$ITEM)
-# pofilter --nonotes --nofuzzy --excludefilter=untranslated -i $ITEM -o $TMP/$ITEM
-# touch $TMP/$ITEM
-# fi
-# done
-#
-# find $TMP/l10n/po -name "*.po" | xargs msgcat --force-po --to-code=utf-8 --no-wrap --use-first -o $TMP/accumulation.po -i $TMP/accumulation.po
-#
-# grep -v "^#~" $TMP/accumulation.po > $TMP/comment-filtered.po
-#
-# po2tmx -l $TARGET_LANG -i $TMP/comment-filtered.po -o $TMX_FILE_PATH
-# bin/sort-tmx
